44c17d894dc6ffd305493135dfbbf5db9dfb6048
SaMuRaIchI <mongushsayin@mail.ru>
2024-07-31 23:04:40 +0700

    Add files via upload

--- suki.py
+++ suki.py
@@ -2,11 +2,14 @@ name = 'SamuRaIchI'
 print(name)
 age = 20
 print(age)
-age1 = 20+1
-print(age1)
 new_age = 21
 print(new_age)
-is_student = 20<21
-print(is_student)
 is_student = age<new_age
+print(is_student)
+
+
+
+age = 20+1
+print(age)
+is_student = 20<21
 print(is_student)
