4470b054c05e508aa7e9d01f546f29c9825e7666
SaMuRaIchI <mongushsayin@mail.ru>
2024-07-31 23:02:00 +0700

    Add files via upload

--- suki.py
+++ suki.py
@@ -1,6 +1,12 @@
-name = 'Moroko'
-print(name[0])
-print(name[-1])
-print(name[3:])
-print(name[::-1])
-print(name[1:6:2])
+name = 'SamuRaIchI'
+print(name)
+age = 20
+print(age)
+age1 = 20+1
+print(age1)
+new_age = 21
+print(new_age)
+is_student = 20<21
+print(is_student)
+is_student = age<new_age
+print(is_student)
