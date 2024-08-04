1d66aaf76fac26ae859f62ea0f6bcafeca6db511
SaMuRaIchI <mongushsayin@mail.ru>
2024-08-04 23:20:47 +0700

    Add files via upload

--- suki.py
+++ suki.py
@@ -1,7 +1,14 @@
-immutable_var = 1,2,4,'clovo'       #Значение элемента кортежа можно изменить тольео если он в []
-print(immutable_var)                                       #В других случаях он не изменяем
-mutable_list = [1,2],3,'clovo'
-print(mutable_list)
-mutable_list[0][1] = 'samka'
-mutable_list[0][0] = 4
-print(mutable_list)
+date_ = {'Sandi':1997,'Sara':2001,'Andrei':2003}
+print(date_)
+print(date_.get('Andrei'))
+print(date_.get('Max','Ошибка ввода!'))
+date_.update({'Anton':2002,
+               'Dana':2004})
+print(date_.pop('Sandi'))
+print(date_)
+
+my_set = {1, 2, 5, 4, 3, 2, 5, 65, 345, 76, 3, 1, 8, 5, 4}
+print(my_set)
+my_set.update({40,26})
+my_set.remove(76)
+print(my_set)
